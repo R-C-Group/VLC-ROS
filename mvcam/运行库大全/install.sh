5591b7d40444a43acf4683c4f1a1e20212f0b067
陈世桓 <31784653+ChenShihuan@users.noreply.github.com>
2020-07-28 01:14:42 +0800

    Update install.sh

--- mvcam/运行库大全/install.sh
+++ mvcam/运行库大全/install.sh
@@ -29,6 +29,11 @@ elif [ $B = "armv7l" ]; then
 	cp armhf/libMVSDK.so  /lib
 	cp armhf/libMVSDK.so  ../lib
 	echo "Copy armhf 32bit libMVSDK.so to /lib and ../lib \n"
+elif [ $B = "aarch64" ]; then
+	cp armhf/libMVSDK.so  /lib
+	cp armhf/libMVSDK.so  ../lib
+	echo "Copy armhf 32bit libMVSDK.so to /lib and ../lib \n But I'm not sure if it's going to work on aarch64 architecture, if it works, remove this message.
+"
 else
 	echo "Not support"
 	exit 1;
